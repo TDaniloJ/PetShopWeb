41b1afb461ddb3909620fa05c4d823e4ba72adff
Danilo Dias <131262817+TDaniloJ@users.noreply.github.com>
2024-03-20 11:13:46 -0300

    Add files via upload

--- js/produtoResumo.js
+++ js/produtoResumo.js
@@ -1,1 +1,5 @@
+const valor1 = document.getElementById("valor1").textContent();
+const valor2 = document.getElementById("valor2").textContent();
 
+const Total = valor1 + valor2;
+document.getElementById("Total").innerHTML = Total;
